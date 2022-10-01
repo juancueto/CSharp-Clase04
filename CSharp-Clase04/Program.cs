ac8286ab24b45d8682aef27d318f4183020ce37f
juancueto <juan.cueto@outlook.com>
2022-10-01 13:26:22 -0500

    Ejercicios 1, 2, 3 y reto

---
+++ CSharp-Clase04/Program.cs
@@ -0,0 +1,136 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Ejercicio 1
+Console.WriteLine("Ingrese un numero entero:");
+int numero = int.Parse(Console.ReadLine());
+int multiplicador = 1;
+while (multiplicador <= 10)
+{
+    Console.WriteLine($"{numero} x {multiplicador} = {numero * multiplicador}");
+    multiplicador++;
+}
+
+// Ejercicio 2
+
+string texto = "";
+do
+{
+    Console.WriteLine("Ingrese un numero:");
+    texto = Console.ReadLine();
+    int numero = 0;
+    if (int.TryParse(texto, out numero))
+    {
+        if (numero > 0)
+            Console.WriteLine("Es positivo");
+        else if (numero == 0)
+            Console.WriteLine("Es cero");
+        else
+            Console.WriteLine("Es negativo");
+    }
+    else
+    {
+        Console.WriteLine("Numero no valido. Fin.");
+    }
+}
+while (texto != "");
+
+// Ejercicio 3
+
+Console.WriteLine("Ingrese el ancho:");
+int ancho = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el alto:");
+int alto = int.Parse(Console.ReadLine());
+Console.WriteLine("Relleno?:");
+bool esRelleno = bool.Parse(Console.ReadLine());
+
+for (int q = 0; q < alto; q++)
+{
+    if (esRelleno)
+    {
+        for (int i = 0; i < ancho; i++)
+        {
+            Console.Write("*");
+        }
+    }
+    else
+    {
+        if (q == 0 || q == alto - 1)
+        {
+            for (int i = 0; i < ancho; i++)
+            {
+                Console.Write("*");
+            }
+        }
+        else
+        {
+            Console.Write("*");
+            for (int i = 1; i < ancho - 1; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("*");
+        }
+
+    }
+
+    Console.Write("\n");
+}
+
+//Reto:
+
+Console.WriteLine("Ingrese el ancho:");
+int ancho = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el alto:");
+int alto = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Ingrese el numero de divisiones:");
+int divisiones = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Relleno?:");
+bool esRelleno = bool.Parse(Console.ReadLine());
+
+for (int q = 0; q < alto; q++)
+{
+    for(int j = 0; j < divisiones; j++)
+    {
+        if (esRelleno)
+        {
+            for (int i = 0; i < ancho; i++)
+            {
+                Console.Write("*");
+            }
+        }
+        else
+        {
+            if (q == 0 || q == alto - 1)
+            {
+                for (int i = 0; i < ancho; i++)
+                {
+                    Console.Write("*");
+                }
+            }
+            else
+            {
+                Console.Write("*");
+                for (int i = 1; i < ancho - 1; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+            }
+
+        }
+
+        Console.Write(" ");
+    
+    }
+
+    Console.Write("\n");
+}
+
+
+
+
+
